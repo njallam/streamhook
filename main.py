3ebd70cfafe1a88244287c71ad97c5c7e85327e5
Nicholas Allam <njallam@outlook.com>
2021-05-11 07:34:23 +0100

    Use requests session for Discord webhook

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import json
 import os
+import requests
 import shelve
 import signal
 import sys
 import time
 import twitch
-from datetime import datetime
-from discord_webhook import DiscordEmbed, DiscordWebhook
 from dotenv import load_dotenv
 
 signal.signal(signal.SIGINT, lambda *args: sys.exit(0))
@@ -17,8 +16,29 @@ with open("streamers.json") as f:
     streamers = json.load(f)
 
 db = shelve.open("shelve.db")
+discord_session = requests.Session()
 
-helix = twitch.Helix(os.environ.get("TWITCH_CLIENT_ID"), os.environ.get("TWITCH_CLIENT_SECRET"))
+helix = twitch.Helix(
+    os.environ.get("TWITCH_CLIENT_ID"), os.environ.get("TWITCH_CLIENT_SECRET")
+)
+
+
+def create_webhook(url, webhook):
+    return discord_session.post(url, json=webhook, params={"wait": True})
+
+
+def edit_webhook(url, id, webhook):
+    return discord_session.patch(f"{url}/messages/{id}", json=webhook)
+
+
+def edit_was_live(user_login, streamer):
+    webhook = {"content": streamer["was_live_message"], 'embeds':[]}
+    try:
+        edit_webhook(streamer["webhook_url"], db[k]["message_id"], webhook)
+        del db[user_login]
+    except:
+        print("Webhook edit failed")
+
 
 while True:
     streams = {
@@ -28,55 +48,59 @@ while True:
 
     for k, v in streamers.items():
         if k in streams.keys():
-            stream = streams[k]
             stream_url = f"https://twitch.tv/{k}"
-            webhook = DiscordWebhook(
-                url=v["webhook_url"], content=v["now_live_message"]
-            )
-            preview_image = stream.thumbnail_url.replace("{width}", "426").replace(
-                "{height}", "240"
-            )
+            stream = streams[k]
             started_at = stream.data["started_at"]
-            embed = DiscordEmbed(title=stream.title, color="6441a4", timestamp=stream.data["started_at"], url=stream_url)
-            embed.set_image(url=preview_image)
-            embed.add_embed_field(
-                name=":busts_in_silhouette: Viewers",
-                value=stream.viewer_count,
-                inline=True,
-            )
-            embed.add_embed_field(
-                name=":joystick: Category", value=stream.data["game_name"], inline=True
-            )
-            webhook.add_embed(embed)
+            webhook = {
+                "content": v["now_live_message"],
+                "embeds": [
+                    {
+                        "title": stream.title,
+                        "color": 0x6441a4,
+                        "timestamp": started_at,
+                        "url": stream_url,
+                        "image": {
+                            "url": stream.thumbnail_url.replace(
+                                "{width}", "426"
+                            ).replace("{height}", "240")
+                        },
+                        "fields": [
+                            {
+                                "name": ":busts_in_silhouette: Viewers",
+                                "value": stream.viewer_count,
+                                "inline": True,
+                            },
+                            {
+                                "name": ":joystick: Category",
+                                "value": stream.data["game_name"],
+                                "inline": True,
+                            },
+                        ],
+                    }
+                ],
+            }
             if k in db.keys() and started_at == db[k]["started_at"]:
                 print(f"{k} | STILL LIVE")
                 try:
-                    response = webhook.edit(db[k]["sent_webhook"])
+                    response = edit_webhook(v["webhook_url"], db[k]["message_id"], webhook)
                     if response.status_code == 404:
                         del db[k]
                 except:
                     print("Webhook edit failed")
             if k not in db.keys():
                 print(f"{k} | NOW LIVE")
-                if k in db.keys() and "sent_webhook" in db[k]:
+                if k in db.keys() and "message_id" in db[k]:
                     edit_was_live(k, v)
                 try:
-                    response = webhook.execute()
-                    db[k] = {"sent_webhook": response, "started_at": started_at}
+                    response = create_webhook(v["webhook_url"], webhook)
+                    db[k] = {
+                        "message_id": response.json()["id"],
+                        "started_at": started_at,
+                    }
                 except:
                     print("Webhook create failed")
         else:
-            if k in db.keys() and "sent_webhook" in db[k]:
+            if k in db.keys() and "message_id" in db[k]:
                 print(f"{k} | WAS LIVE")
                 edit_was_live(k, v)
     time.sleep(30)
-
-def edit_was_live(user_login, streamer):
-    webhook = DiscordWebhook(
-        url=streamer["webhook_url"], content=streamer["was_live_message"]
-    )
-    try:
-        webhook.update(db[user_login]["sent_webhook"])
-        del db[user_login]
-    except:
-        print("Webhook edit failed")
