d684f0cdd64b6bd4ed10231fd49c2add85eca4f4
Nicholas Allam <njallam@outlook.com>
2021-07-26 18:49:37 +0100

    Refactor & sleep on API error

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ import sys
 import time
 import twitch
 
-logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 signal.signal(signal.SIGINT, lambda *args: sys.exit(0))
 
 try:
@@ -48,53 +50,44 @@ def edit_was_live(user_login, streamer):
     except:
         logging.warning("Webhook edit failed")
 
-logging.info("Now running")
 
-while True:
-    streams = {}
-    try:
-        streams = {
-            stream.data["user_login"]: stream
-            for stream in helix.streams(user_login=streamers.keys())
-        }
-    except twitch.helix.resources.streams.StreamNotFound:
-        pass
-    except:
-        logging.warning("Get streams failed")
+def get_webhook(user_login, streamer, stream):
+    return {
+        "content": streamer["now_live_message"],
+        "embeds": [
+            {
+                "title": stream.title,
+                "color": 0x6441A4,
+                "timestamp": stream.data["started_at"],
+                "url": f"https://twitch.tv/{user_login}",
+                "image": {
+                    "url": stream.thumbnail_url.replace("{width}", "426").replace(
+                        "{height}", "240"
+                    )
+                },
+                "fields": [
+                    {
+                        "name": ":busts_in_silhouette: Viewers",
+                        "value": stream.viewer_count,
+                        "inline": True,
+                    },
+                    {
+                        "name": ":joystick: Category",
+                        "value": stream.data["game_name"],
+                        "inline": True,
+                    },
+                ],
+            }
+        ],
+    }
 
+
+def update_webhooks(streams):
     for user_login, streamer in streamers.items():
         if user_login in streams.keys():
             stream = streams[user_login]
-            started_at = stream.data["started_at"]
-            webhook = {
-                "content": streamer["now_live_message"],
-                "embeds": [
-                    {
-                        "title": stream.title,
-                        "color": 0x6441A4,
-                        "timestamp": started_at,
-                        "url": f"https://twitch.tv/{user_login}",
-                        "image": {
-                            "url": stream.thumbnail_url.replace(
-                                "{width}", "426"
-                            ).replace("{height}", "240")
-                        },
-                        "fields": [
-                            {
-                                "name": ":busts_in_silhouette: Viewers",
-                                "value": stream.viewer_count,
-                                "inline": True,
-                            },
-                            {
-                                "name": ":joystick: Category",
-                                "value": stream.data["game_name"],
-                                "inline": True,
-                            },
-                        ],
-                    }
-                ],
-            }
-            if user_login in db.keys() and started_at == db[user_login]["started_at"]:
+            webhook = get_webhook(user_login, streamer, stream)
+            if user_login in db.keys() and stream.data["started_at"] == db[user_login]["started_at"]:
                 logging.info(f"{user_login} | STILL LIVE")
                 try:
                     response = edit_webhook(
@@ -113,7 +106,7 @@ while True:
                     response = create_webhook(streamer["webhook_url"], webhook)
                     db[user_login] = {
                         "message_id": response.json()["id"],
-                        "started_at": started_at,
+                        "started_at": stream.data["started_at"],
                     }
                 except:
                     logging.warning("Webhook create failed")
@@ -121,4 +114,24 @@ while True:
             if user_login in db.keys() and "message_id" in db[user_login]:
                 logging.info(f"{user_login} | WAS LIVE")
                 edit_was_live(user_login, streamer)
+
+
+logging.info("Now running")
+
+while True:
+    streams = {}
+    try:
+        streams = {
+            stream.data["user_login"]: stream
+            for stream in helix.streams(user_login=streamers.keys())
+        }
+    except twitch.helix.resources.streams.StreamNotFound:
+        pass
+    except:
+        logging.warning("Get streams failed")
+        time.sleep(60)
+        continue
+
+    update_webhooks(streams)
+
     time.sleep(30)
